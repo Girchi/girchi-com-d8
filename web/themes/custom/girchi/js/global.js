ee9a82ceb06d4f537a962c63c40ecc882fad25da
otarza <otar.zakalashvili@gmail.com>
2019-06-01 13:41:03 +0400

    #191 - Adding drop-down menu markup. - Cleaning up global.js

--- web/themes/custom/girchi/js/global.js
+++ web/themes/custom/girchi/js/global.js
@@ -1,7 +1,4 @@
-console.log(123);
 $('#edit-field-politician-value').on('change', (e) => {
-    console.log("123");
-    console.log(e.target);
     if (e.target.checked) {
         $('.form-checkbox-input').addClass('checked');
     } else {
