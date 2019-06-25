ee40cdbd36cda6b50a45ada5c29cf6ce08144af5
GGKapanadze <kapanadzeg41@gmail.com>
2019-06-25 17:18:09 +0400

    #310 Use drupal standarts in girchi_utils

--- web/modules/custom/girchi_utils/src/UpdatePageViews.php
+++ web/modules/custom/girchi_utils/src/UpdatePageViews.php
@@ -6,12 +6,16 @@ use Drupal\Core\Language\LanguageInterface;
 use Google_Client;
 use Google_Service_AnalyticsReporting;
 use Google_Service_AnalyticsReporting_GetReportsRequest;
-use Google_Service_AnalyticsReporting_GetReportsResponse;
 
-class UpdatePageViews
-{
-  private static function _apiCall($url)
-  {
+/**
+ *
+ */
+class UpdatePageViews {
+
+  /**
+   *
+   */
+  private static function _apiCall($url) {
     $reports = self::_getReports($url);
     $views = self::_getViews($reports);
 
@@ -21,11 +25,11 @@ class UpdatePageViews
   /**
    * Initializes an Analytics Reporting API V4 service object.
    *
-   * @return Google_Service_AnalyticsReporting An authorized Analytics Reporting API V4 service object.
+   * @return \Google_Service_AnalyticsReporting An authorized Analytics Reporting API V4 service object.
+   *
    * @throws \Google_Exception
    */
-  private static function _initializeAnalytics()
-  {
+  private static function _initializeAnalytics() {
     $KEY_FILE_LOCATION = __DIR__ . '/../service-account-credentials.json';
 
     // Create and configure a new client object.
@@ -41,14 +45,14 @@ class UpdatePageViews
   /**
    * Get page view reports.
    *
-   * @param $url Page URL.
+   * @param $url
+   *   Page URL.
    *
    * @return \Google_Service_AnalyticsReporting_GetReportsResponse Report Response
    *
    * @throws \Google_Exception
    */
-  private static function _getReports($url)
-  {
+  private static function _getReports($url) {
     $analytics = self::_initializeAnalytics();
 
     $viewId = \Drupal::config('om_site_settings.site_settings')->get('google_analytics_view_id');
@@ -60,22 +64,23 @@ class UpdatePageViews
         "endDate" => 'today',
       ],
       "metrics" => [
-        "expression" => "ga:pageviews"
+        "expression" => "ga:pageviews",
       ],
       "dimensions" => [
-        "name" => "ga:pagepath"
+        "name" => "ga:pagepath",
       ],
       "dimensionFilterClauses" => [
         'filters' => [
           "dimension_name" => "ga:pagepath",
-          "operator" => "PARTIAL", // valid operators can be found here: https://developers.google.com/analytics/devguides/reporting/core/v4/rest/v4/reports/batchGet#FilterLogicalOperator
+      // Valid operators can be found here: https://developers.google.com/analytics/devguides/reporting/core/v4/rest/v4/reports/batchGet#FilterLogicalOperator
+          "operator" => "PARTIAL",
           "expressions" => $url,
-        ]
-      ]
+        ],
+      ],
     ];
 
     $body = new Google_Service_AnalyticsReporting_GetReportsRequest();
-    $body->setReportRequests(array($query));
+    $body->setReportRequests([$query]);
 
     // batchGet the results https://developers.google.com/analytics/devguides/reporting/core/v4/rest/v4/reports/batchGet
     $report = $analytics->reports->batchGet($body);
@@ -86,13 +91,12 @@ class UpdatePageViews
   /**
    * Get page view count.
    *
-   * @param Google_Service_AnalyticsReporting_GetReportsResponse $reports
-   *    Google Analytics report object.
+   * @param \Google_Service_AnalyticsReporting_GetReportsResponse $reports
+   *   Google Analytics report object.
    *
    * @return int|null Page views or null.
    */
-  private static function _getViews($reports)
-  {
+  private static function _getViews($reports) {
     $rows = $reports[0]->getData()->getRows();
 
     if ($rows) {
@@ -103,26 +107,28 @@ class UpdatePageViews
       }
     }
 
-    return null;
+    return NULL;
   }
 
-  public static function updateViews($nids, &$context)
-  {
+  /**
+   *
+   */
+  public static function updateViews($nids, &$context) {
     $message = t('Updating Views...');
-    $results = array();
+    $results = [];
 
     /** @var \Drupal\Core\Database\Connection $database */
     $database = \Drupal::service('database');
 
     foreach ($nids as $nid) {
-      $path = '/node/' . (int)$nid;
+      $path = '/node/' . (int) $nid;
       $langcode = \Drupal::languageManager()->getCurrentLanguage(LanguageInterface::TYPE_CONTENT)->getId();
 
       $url = \Drupal::service('path.alias_manager')->getAliasByPath($path, $langcode);
 
       $viewsCount = self::_apiCall('/ge' . $url);
 
-      if ($viewsCount !== null) {
+      if ($viewsCount !== NULL) {
         $results[$url] = $viewsCount;
 
         $currentState = $database->query(
@@ -143,14 +149,15 @@ class UpdatePageViews
               ':curent_time' => time(),
             ]
           );
-        } else {
+        }
+        else {
           $database->query(
             "UPDATE `girchidrpl_node_counter` SET `totalcount` = :views_count WHERE `nid` = :nid;",
             [
               ':views_count' => $viewsCount,
               ':nid' => $nid,
             ]
-          );
+                  );
         }
       }
     }
@@ -159,14 +166,17 @@ class UpdatePageViews
     $context['results'] = $results;
   }
 
-  public static function updateViewsFinishedCallback($success, $results, $operations)
-  {
+  /**
+   *
+   */
+  public static function updateViewsFinishedCallback($success, $results, $operations) {
     if ($success) {
       $message = \Drupal::translation()->formatPlural(
         count($results),
         'One node views has been updated.', '@count nodes views has been updated.'
       );
-    } else {
+    }
+    else {
       $message = t('Finished with an error.');
     }
 
